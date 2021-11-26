efca7d3bc2dcd0e87bb2917bb84b462e0bcd1953
amar2006037 <90335297+amar2006037@users.noreply.github.com>
2021-11-26 18:43:02 +0530

    Add files via upload

---
+++ JS_ASS.js
@@ -0,0 +1,9 @@
+let Person={
+    FirstName:"Amar",
+    LastName:"Ram",
+    'Office Address':"Patna",
+};
+console.log(Person.FirstName);
+console.log(Person.LastName);
+console.log(Person['Office Address']);
+console.log(Person);
